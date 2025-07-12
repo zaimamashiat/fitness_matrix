b9330936e465f8706a73347d8ef22ee758f381d3
zaimamashiat <153401737+zaimamashiat@users.noreply.github.com>
2025-07-12 22:28:03 +0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: "https://fitness-matrix-delta.vercel.app",
+  credentials: true
+}));
+
 app.use(express.json()); // Parse JSON requests
 
 // Database connection
